c6926cbde4d0846e1d516934ad1275fea2c2492e
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-06-21 08:57:13 -0300

    mapeamento de outros atributos

--- jpa_hibernate_loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ jpa_hibernate_loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -5,15 +5,14 @@ import java.math.BigDecimal;
 import javax.persistence.EntityManager;
 
 import br.com.alura.loja.dao.ProdutoDAO;
+import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JpaUtil;
 
 public class CadastroDeProduto {
 	public static void main(String[] args) {
-		Produto celular = new Produto();
-		celular.setNome("iPhone 14 Pro Max 1TB");
-		celular.setDescricao("O melhor de todos");
-		celular.setPreco(new BigDecimal("12000"));
+		Produto celular = new Produto("iPhone 14 Pro Max 1TB", "O melhor de todos", new BigDecimal("12000"),
+				Categoria.CELULARES);
 
 		EntityManager em = JpaUtil.getEntityManager();
 		ProdutoDAO produtoDao = new ProdutoDAO(em);

a2deb545e260044c2e05868e5cfba89836f4356b
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-06-22 07:55:52 -0300

    mapeamento de relacionamentos

---
+++ jpa_hibernate_loja/src/main/java/br/com/alura/loja/dao/CategoriaDAO.java
@@ -0,0 +1,19 @@
+package br.com.alura.loja.dao;
+
+import javax.persistence.EntityManager;
+
+import br.com.alura.loja.modelo.Categoria;
+
+public class CategoriaDAO {
+
+	private EntityManager em;
+
+	public CategoriaDAO(EntityManager em) {
+		this.em = em;
+	}
+
+	public void cadastra(Categoria categoria) {
+		this.em.persist(categoria);
+	}
+
+}
--- jpa_hibernate_loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ jpa_hibernate_loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
 
+import br.com.alura.loja.dao.CategoriaDAO;
 import br.com.alura.loja.dao.ProdutoDAO;
 import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
@@ -11,13 +12,16 @@ import br.com.alura.loja.util.JpaUtil;
 
 public class CadastroDeProduto {
 	public static void main(String[] args) {
+		Categoria categoriaCelulares = new Categoria("CELULARES");
 		Produto celular = new Produto("iPhone 14 Pro Max 1TB", "O melhor de todos", new BigDecimal("12000"),
-				Categoria.CELULARES);
+				categoriaCelulares);
 
 		EntityManager em = JpaUtil.getEntityManager();
+		CategoriaDAO categoriaDao = new CategoriaDAO(em);
 		ProdutoDAO produtoDao = new ProdutoDAO(em);
 
 		em.getTransaction().begin();
+		categoriaDao.cadastra(categoriaCelulares);
 		produtoDao.cadastra(celular);
 		em.getTransaction().commit();
 		em.close();

f6c99df0162f463f5b42e197a98b63271b4f9a98
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-07-10 11:11:06 -0300

    listar dados e acessar variaveis na pagina com Thymeleaf

--- spring_screenmatch/src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
+++ spring_screenmatch/src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,12 +23,17 @@ public class FilmeController {
 		return "filmes/formulario";
 	}
 
+	@GetMapping("/listagem")
+	public String carregaPaginaListagem(Model model) {
+		model.addAttribute("lista", listaDeFilmesCadastrados);
+		return "filmes/listagem";
+	}
+
 	@PostMapping("/cadastro")
 	public String cadastraFilme(DadosCadastroFilme dados) {
 		Filme filme = new Filme(dados);
 		listaDeFilmesCadastrados.add(filme);
-		System.out.println(listaDeFilmesCadastrados);
 
-		return "filmes/formulario";
+		return "redirect:/filmes/listagem";
 	}
 }

8b49dde238e86d240cb8bd80a94d645860daae9e
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-07-10 10:27:02 -0300

    criar formulario e cadastrar dados (anotacao @PostMapping)

--- spring_screenmatch/src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
+++ spring_screenmatch/src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -1,15 +1,33 @@
 package br.com.alura.screenmatch.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.screenmatch.domain.filme.DadosCadastroFilme;
+import br.com.alura.screenmatch.domain.filme.Filme;
 
 @Controller
 @RequestMapping("/filmes")
 public class FilmeController {
 
+	private List<Filme> listaDeFilmesCadastrados = new ArrayList<>();
+
 	@GetMapping("/cadastro")
 	public String carregaPaginaFormulario() {
+		return "filmes/formulario";
+	}
+
+	@PostMapping("/cadastro")
+	public String cadastraFilme(DadosCadastroFilme dados) {
+		Filme filme = new Filme(dados);
+		listaDeFilmesCadastrados.add(filme);
+		System.out.println(listaDeFilmesCadastrados);
+
 		return "filmes/formulario";
 	}
 }

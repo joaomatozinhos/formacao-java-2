0bb0472a4d24d95733b1a482e7699f4457313784
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-07-12 08:14:13 -0300

    operacoes CRUD no banco de dados

--- spring_screenmatch/src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
+++ spring_screenmatch/src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -3,13 +3,17 @@ package br.com.alura.screenmatch.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.alura.screenmatch.domain.filme.DadosAlteracaoFilme;
 import br.com.alura.screenmatch.domain.filme.DadosCadastroFilme;
 import br.com.alura.screenmatch.domain.filme.Filme;
 import br.com.alura.screenmatch.domain.filme.FilmeRepository;
+import jakarta.transaction.Transactional;
 
 @Controller
 @RequestMapping("/filmes")
@@ -19,7 +23,11 @@ public class FilmeController {
 	private FilmeRepository repository;
 
 	@GetMapping("/cadastro")
-	public String carregaPaginaFormulario() {
+	public String carregaPaginaFormulario(Long id, Model model) {
+		if (id != null) {
+			Filme filme = repository.getReferenceById(id);
+			model.addAttribute("filme", filme);
+		}
 		return "filmes/formulario";
 	}
 
@@ -30,10 +38,28 @@ public class FilmeController {
 	}
 
 	@PostMapping("/cadastro")
+	@Transactional
 	public String cadastraFilme(DadosCadastroFilme dados) {
 		Filme filme = new Filme(dados);
 		repository.save(filme);
 
+		return "redirect:/filmes/listagem";
+	}
+
+	@PutMapping("/cadastro")
+	@Transactional
+	public String editaFilme(DadosAlteracaoFilme dados) {
+		Filme filme = repository.getReferenceById(dados.id());
+		filme.atualizaDados(dados);
+		
+
+		return "redirect:/filmes/listagem";
+	}
+
+	@DeleteMapping("/listagem")
+	@Transactional
+	public String removeFilme(Long id) {
+		repository.deleteById(id);
 		return "redirect:/filmes/listagem";
 	}
 }

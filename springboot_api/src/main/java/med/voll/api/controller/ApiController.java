c267e2df48ad1051759790c0107caf8836a9cb7d
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-07-13 08:41:36 -0300

    codigo inicial curso spring boot: projeto de uma api

---
+++ springboot_api/src/main/java/med/voll/api/controller/ApiController.java
@@ -0,0 +1,16 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+	@GetMapping
+	public String testaApi() {
+		return "Teste da API realizado com sucesso!";
+	}
+
+}

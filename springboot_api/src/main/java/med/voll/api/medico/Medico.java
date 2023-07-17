ffb975d5d804a57e370f65e98d9095e8977c9ace
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-07-17 09:19:02 -0300

    criar uma nova migration (flyway)

--- springboot_api/src/main/java/med/voll/api/medico/Medico.java
+++ springboot_api/src/main/java/med/voll/api/medico/Medico.java
@@ -19,6 +19,7 @@ public class Medico {
 	private Long id;
 	private String nome;
 	private String email;
+	private String telefone;
 	private String crm;
 
 	@Enumerated(EnumType.STRING)
@@ -33,6 +34,7 @@ public class Medico {
 	public Medico(DadosCadastroMedico dados) {
 		this.nome = dados.nome();
 		this.email = dados.email();
+		this.telefone = dados.telefone();
 		this.crm = dados.crm();
 		this.especialidade = dados.especialidade();
 		this.endereco = new Endereco(dados.endereco());
@@ -48,6 +50,10 @@ public class Medico {
 
 	public String getEmail() {
 		return email;
+	}
+
+	public String getTelefone() {
+		return telefone;
 	}
 
 	public String getCrm() {

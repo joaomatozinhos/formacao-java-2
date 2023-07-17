a0a19ec347f200ea23befabdcd5fbc154259eafd
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-07-17 07:58:37 -0300

    validacao de campos com Bean Validation

--- springboot_api/src/main/java/med/voll/api/controller/MedicoController.java
+++ springboot_api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -20,7 +21,7 @@ public class MedicoController {
 
 	@PostMapping("/cadastro")
 	@Transactional
-	public void cadastra(@RequestBody DadosCadastroMedico dados) {
+	public void cadastra(@RequestBody @Valid DadosCadastroMedico dados) {
 		Medico medico = new Medico(dados);
 		repository.save(medico);
 	}

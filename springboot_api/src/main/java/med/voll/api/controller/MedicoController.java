333428871a3b0705d46d1d63f07028bf5ffdf3ab
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-07-18 07:47:59 -0300

    buscar dados (requisicao GET) com Spring

--- springboot_api/src/main/java/med/voll/api/controller/MedicoController.java
+++ springboot_api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,9 @@
 package med.voll.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 
@@ -24,6 +28,11 @@ public class MedicoController {
 	public void cadastra(@RequestBody @Valid DadosCadastroMedico dados) {
 		Medico medico = new Medico(dados);
 		repository.save(medico);
+	}
+
+	@GetMapping("/listagem")
+	public List<DadosListagemMedico> listaTodos() {
+		return repository.findAll().stream().map(medico -> new DadosListagemMedico(medico)).toList();
 	}
 
 }

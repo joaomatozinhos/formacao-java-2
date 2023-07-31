25a7a0a1b83ceaef498b1e8d03da5a12c7715588
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-07-31 19:17:26 -0300

    boa pratica: tratamento do retorno da API

--- springboot_api/src/main/java/med/voll/api/controller/PacienteController.java
+++ springboot_api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,9 +1,12 @@
 package med.voll.api.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +15,13 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosAtualizacaoPaciente;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosDetalhamentoPaciente;
 import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
@@ -30,26 +35,36 @@ public class PacienteController {
 
 	@PostMapping("/cadastro")
 	@Transactional
-	public void cadastro(@RequestBody @Valid DadosCadastroPaciente dados) {
+	public ResponseEntity<DadosDetalhamentoPaciente> cadastro(@RequestBody @Valid DadosCadastroPaciente dados,
+			UriComponentsBuilder uriBuilder) {
 		Paciente paciente = new Paciente(dados);
 		repository.save(paciente);
+
+		URI uri = uriBuilder.path("/pacientes/cadastro/{id}").buildAndExpand(paciente.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(new DadosDetalhamentoPaciente(paciente));
 	}
 
 //	@GetMapping("/listagem")
 //	public List<DadosListagemPaciente> lista() {
 //		return repository.findAll().stream().map(DadosListagemPaciente::new).toList();
 //	}
-	
-	@GetMapping
-	public Page<DadosListagemPaciente> listar(@PageableDefault(page = 0, size = 10, sort = { "nome" }) Pageable paginacao) {
-	    return repository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
+
+	@GetMapping("/listagem")
+	public ResponseEntity<Page<DadosListagemPaciente>> lista(
+			@PageableDefault(page = 0, size = 10, sort = { "nome" }) Pageable paginacao) {
+		Page<DadosListagemPaciente> page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
+
+		return ResponseEntity.ok(page);
 	}
 
 	@PutMapping("/edicao")
 	@Transactional
-	public void atualiza(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
+	public ResponseEntity<DadosDetalhamentoPaciente> atualiza(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
 		Paciente paciente = repository.getReferenceById(dados.id());
 		paciente.atualiza(dados);
+
+		return ResponseEntity.ok(new DadosDetalhamentoPaciente(paciente));
 	}
 
 //	@DeleteMapping("/exclusao/{id}")
@@ -60,8 +75,11 @@ public class PacienteController {
 
 	@DeleteMapping("/exclusao/{id}")
 	@Transactional
-	public void exclui(@PathVariable Long id) {
+	public ResponseEntity<Void> exclui(@PathVariable Long id) {
 		Paciente paciente = repository.getReferenceById(id);
 		paciente.exclui();
+
+		return ResponseEntity.noContent().build();
+
 	}
 }

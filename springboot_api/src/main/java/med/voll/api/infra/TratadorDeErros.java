167bb1ce7bf97c78a132000c0e2dffb0f3399401
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-08-02 07:33:42 -0300

    tratamento de erros: tratar erro 400

--- springboot_api/src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ springboot_api/src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -1,6 +1,10 @@
 package med.voll.api.infra;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +16,20 @@ public class TratadorDeErros {
 	@ExceptionHandler(EntityNotFoundException.class)
 	public ResponseEntity<Void> trataErro404() {
 		return ResponseEntity.notFound().build();
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<List<DadosErroValidacao>> trataErro400(MethodArgumentNotValidException ex) {
+		List<FieldError> erros = ex.getFieldErrors();
+
+		return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+
+	}
+
+	private record DadosErroValidacao(String campo, String mensagem) {
+		public DadosErroValidacao(FieldError erro) {
+			this(erro.getField(), erro.getDefaultMessage());
+		}
 	}
 
 }

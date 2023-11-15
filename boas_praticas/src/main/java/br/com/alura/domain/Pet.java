825b1f9b12835fbd26175b7153965c53d70a5b4d
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-11-15 05:49:43 -0300

    lib Jackson para simplificar serialização e deserialização de objetos

--- boas_praticas/src/main/java/br/com/alura/domain/Pet.java
+++ boas_praticas/src/main/java/br/com/alura/domain/Pet.java
@@ -10,6 +10,9 @@ public class Pet {
 	private String cor;
 	private Float peso;
 
+	public Pet() {
+	}
+
 	public Pet(String tipo, String nome, String raca, int idade, String cor, Float peso) {
 		this.tipo = tipo;
 		this.nome = nome;
@@ -17,5 +20,33 @@ public class Pet {
 		this.idade = idade;
 		this.cor = cor;
 		this.peso = peso;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getRaca() {
+		return raca;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public Float getPeso() {
+		return peso;
 	}
 }

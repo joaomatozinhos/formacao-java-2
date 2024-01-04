e822c82d39ec341206eed5c3bc19bcd42e54c109
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2024-01-04 16:07:16 -0300

    refatoracao completa de uma funcionalidade

--- boas_praticas_api/src/main/java/br/com/alura/adopet/api/model/Tutor.java
+++ boas_praticas_api/src/main/java/br/com/alura/adopet/api/model/Tutor.java
@@ -1,91 +1,81 @@
 package br.com.alura.adopet.api.model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-
 import java.util.List;
 import java.util.Objects;
+
+import br.com.alura.adopet.api.dto.AtualizacaoTutorDto;
+import br.com.alura.adopet.api.dto.CadastroTutorDto;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tutores")
 public class Tutor {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @NotBlank
-    @Column(name = "nome")
-    private String nome;
+	private String nome;
 
-    @NotBlank
-    @Pattern(regexp = "\\(?\\d{2}\\)?\\d?\\d{4}-?\\d{4}")
-    @Column(name = "telefone")
-    private String telefone;
+	private String telefone;
 
-    @NotBlank
-    @Email
-    @Column(name = "email")
-    private String email;
+	private String email;
 
-    @OneToMany(mappedBy = "tutor")
-    @JsonManagedReference("tutor_adocoes")
-    private List<Adocao> adocoes;
+	@OneToMany(mappedBy = "tutor")
+	private List<Adocao> adocoes;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Tutor tutor = (Tutor) o;
-        return Objects.equals(id, tutor.id);
-    }
+	public Tutor() {
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+	public Tutor(CadastroTutorDto dto) {
+		this.nome = dto.nome();
+		this.telefone = dto.telefone();
+		this.email = dto.email();
+	}
 
-    public Long getId() {
-        return id;
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Tutor tutor = (Tutor) o;
+		return Objects.equals(id, tutor.id);
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public String getTelefone() {
-        return telefone;
-    }
+	public String getTelefone() {
+		return telefone;
+	}
 
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public List<Adocao> getAdocoes() {
+		return adocoes;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public List<Adocao> getAdocoes() {
-        return adocoes;
-    }
-
-    public void setAdocoes(List<Adocao> adocoes) {
-        this.adocoes = adocoes;
-    }
+	public void atualizarDados(AtualizacaoTutorDto dto) {
+		this.nome = dto.nome();
+		this.telefone = dto.telefone();
+		this.email = dto.email();
+	}
 }
--- boas_praticas_api/src/main/java/br/com/alura/adopet/api/repository/TutorRepository.java
+++ boas_praticas_api/src/main/java/br/com/alura/adopet/api/repository/TutorRepository.java
@@ -1,12 +1,11 @@
 package br.com.alura.adopet.api.repository;
 
-import br.com.alura.adopet.api.model.Tutor;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.alura.adopet.api.model.Tutor;
 
 public interface TutorRepository extends JpaRepository<Tutor, Long> {
 
-    boolean existsByTelefone(String telefone);
-
-    boolean existsByEmail(String email);
+	boolean existsByTelefoneOrEmail(String telefone, String email);
 
 }

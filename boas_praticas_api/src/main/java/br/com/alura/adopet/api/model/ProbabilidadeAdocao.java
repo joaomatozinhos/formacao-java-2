c52803cc7e4e4adc357f40635cf11692084791f4
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2024-01-08 15:19:38 -0300

    codigo para teste

---
+++ boas_praticas_api/src/main/java/br/com/alura/adopet/api/model/ProbabilidadeAdocao.java
@@ -0,0 +1,5 @@
+package br.com.alura.adopet.api.model;
+
+public enum ProbabilidadeAdocao {
+	BAIXA, MEDIA, ALTA;
+}
---
+++ boas_praticas_api/src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
@@ -0,0 +1,12 @@
+package br.com.alura.adopet.api.service;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraProbabilidadeAdocaoTest {
+
+	@Test
+	void cenario01() {
+
+	}
+
+}

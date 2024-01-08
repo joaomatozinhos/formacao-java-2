5ab2f60d053e58bd46ccaddcad045204ec68d9ff
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2024-01-08 16:14:39 -0300

    escrever um teste automatizado

--- boas_praticas_api/src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
+++ boas_praticas_api/src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
@@ -1,12 +1,29 @@
 package br.com.alura.adopet.api.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+
+import br.com.alura.adopet.api.dto.CadastroAbrigoDto;
+import br.com.alura.adopet.api.dto.CadastroPetDto;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.ProbabilidadeAdocao;
+import br.com.alura.adopet.api.model.TipoPet;
 
 public class CalculadoraProbabilidadeAdocaoTest {
 
 	@Test
-	void cenario01() {
+	void verificarSeGatoCom4AnosE4KgTemProbabilidadeAlta() {
+		// veriidade 4 anos e 4kg - ALTA
 
+		Abrigo abrigo = new Abrigo(new CadastroAbrigoDto("Abrigo feliz", "94999999999", "abrigofeliz@email.com.br"));
+		Pet pet = new Pet(new CadastroPetDto(TipoPet.GATO, "Miau", "Siames", 4, "Cinza", 4.0f), abrigo);
+
+		CalculadoraProbabilidadeAdocao calculadora = new CalculadoraProbabilidadeAdocao();
+		ProbabilidadeAdocao probabilidade = calculadora.calcular(pet);
+
+		assertEquals(ProbabilidadeAdocao.ALTA, probabilidade);
 	}
 
 }

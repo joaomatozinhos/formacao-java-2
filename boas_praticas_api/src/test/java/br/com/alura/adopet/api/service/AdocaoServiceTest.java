4c558f6020747778a82c67493072d51b37e92eb7
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2024-01-12 15:22:14 -0300

    teste automatizado em classe service

--- boas_praticas_api/src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
+++ boas_praticas_api/src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -1,10 +1,15 @@
 package br.com.alura.adopet.api.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -15,10 +20,13 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import br.com.alura.adopet.api.dto.AprovacaoAdocaoDto;
+import br.com.alura.adopet.api.dto.ReprovacaoAdocaoDto;
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.model.Abrigo;
 import br.com.alura.adopet.api.model.Adocao;
 import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.StatusAdocao;
 import br.com.alura.adopet.api.model.Tutor;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
 import br.com.alura.adopet.api.repository.PetRepository;
@@ -28,76 +36,200 @@ import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
 @ExtendWith(MockitoExtension.class)
 class AdocaoServiceTest {
 
-	@InjectMocks
-	private AdocaoService service;
+    @InjectMocks
+    private AdocaoService service;
 
-	@Mock
-	private AdocaoRepository adocaoRepository;
+    @Mock
+    private AdocaoRepository repository;
 
-	@Mock
-	private PetRepository petRepository;
+    @Mock
+    private PetRepository petRepository;
 
-	@Mock
-	private TutorRepository tutorRepository;
+    @Mock
+    private TutorRepository tutorRepository;
 
-	@Mock
-	private EmailService emailService;
+    @Mock
+    private EmailService emailService;
 
-	@Spy
-	private List<ValidacaoSolicitacaoAdocao> validacoes = new ArrayList<>();
+    @Spy
+    private List<ValidacaoSolicitacaoAdocao> validacoes = new ArrayList<>();
 
-	@Mock
-	private ValidacaoSolicitacaoAdocao validador1;
+    @Mock
+    private ValidacaoSolicitacaoAdocao validador1;
 
-	@Mock
-	private ValidacaoSolicitacaoAdocao validador2;
+    @Mock
+    private ValidacaoSolicitacaoAdocao validador2;
 
-	@Mock
-	private Pet pet;
+    @Mock
+    private Pet pet;
 
-	@Mock
-	private Tutor tutor;
+    @Mock
+    private Tutor tutor;
 
-	@Mock
-	private Abrigo abrigo;
+    @Mock
+    private Abrigo abrigo;
 
-	@Mock
-	private SolicitacaoAdocaoDto dto;
+    private SolicitacaoAdocaoDto dto;
 
-	@Captor
-	private ArgumentCaptor<Adocao> adocaoCaptor;
+    @Captor
+    private ArgumentCaptor<Adocao> adocaoCaptor;
 
-	@Test
-	void deveriaSalvarAdocaoAoSolicitar() {
+    @Mock
+    private AprovacaoAdocaoDto aprovacaoAdocaoDto;
 
-		this.dto = new SolicitacaoAdocaoDto(10l, 20l, "motivo qualquer");
-		BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
-		BDDMockito.given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
-		BDDMockito.given(pet.getAbrigo()).willReturn(abrigo);
+    @Mock
+    private ReprovacaoAdocaoDto reprovacaoAdocaoDto;
 
-		service.solicitar(dto);
+    @Spy
+    private Adocao adocao;
 
-		BDDMockito.then(adocaoRepository).should().save(adocaoCaptor.capture());
-		Adocao adocaoSalva = adocaoCaptor.getValue();
+    @Test
+    void deveriaSalvarAdocaoAoSolicitar() {
+        //ARRANGE
+        this.dto = new SolicitacaoAdocaoDto(10l, 20l, "motivo qualquer");
+        given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
+        given(pet.getAbrigo()).willReturn(abrigo);
 
-		assertEquals(pet, adocaoSalva.getPet());
-		assertEquals(tutor, adocaoSalva.getTutor());
-		assertEquals(dto.motivo(), adocaoSalva.getMotivo());
-	}
+        //ACT
+        service.solicitar(dto);
 
-	@Test
-	void deveriaChamarValidadoresAoSolicitar() {
+        //ASSERT
+        then(repository).should().save(adocaoCaptor.capture());
+        Adocao adocaoSalva = adocaoCaptor.getValue();
+        Assertions.assertEquals(pet, adocaoSalva.getPet());
+        Assertions.assertEquals(tutor, adocaoSalva.getTutor());
+        Assertions.assertEquals(dto.motivo(), adocaoSalva.getMotivo());
+    }
 
-		this.dto = new SolicitacaoAdocaoDto(10l, 20l, "motivo qualquer");
-		BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
-		BDDMockito.given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
-		BDDMockito.given(pet.getAbrigo()).willReturn(abrigo);
-		validacoes.add(validador1);
-		validacoes.add(validador2);
+    @Test
+    void deveriaChamarValidadoresDeAdocaoAoSolicitar() {
+        //ARRANGE
+        this.dto = new SolicitacaoAdocaoDto(10l, 20l, "motivo qualquer");
+        given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
+        given(pet.getAbrigo()).willReturn(abrigo);
+        validacoes.add(validador1);
+        validacoes.add(validador2);
 
-		service.solicitar(dto);
+        //ACT
+        service.solicitar(dto);
 
-		BDDMockito.then(validador1).should().validar(dto);
-		BDDMockito.then(validador2).should().validar(dto);
-	}
+        //ASSERT
+        BDDMockito.then(validador1).should().validar(dto);
+        BDDMockito.then(validador2).should().validar(dto);
+    }
+
+    @Test
+    void deveriaEnviarEmailAoSolicitarAdocao() {
+
+        //ARRANGE
+        SolicitacaoAdocaoDto dto = new SolicitacaoAdocaoDto(10l, 30l, "motivo teste");
+        given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
+        given(pet.getAbrigo()).willReturn(abrigo);
+
+        //ACT
+        service.solicitar(dto);
+
+        //ASSERT
+        then(repository).should().save(adocaoCaptor.capture());
+        Adocao adocao = adocaoCaptor.getValue();
+        then(emailService).should().enviarEmail(
+                adocao.getPet().getAbrigo().getEmail(),
+                "Solicitação de adoção",
+                "Olá " +adocao.getPet().getAbrigo().getNome() +"!\n\nUma solicitação de adoção foi registrada hoje para o pet: " +adocao.getPet().getNome() +". \nFavor avaliar para aprovação ou reprovação."
+        );
+    }
+
+    @Test
+    void deveriaAprovarUmaAdocao(){
+
+        //Arrange
+        given(repository.getReferenceById(aprovacaoAdocaoDto.idAdocao())).willReturn(adocao);
+        given(adocao.getPet()).willReturn(pet);
+        given(pet.getAbrigo()).willReturn(abrigo);
+        given(abrigo.getEmail()).willReturn("email@example.com");
+        given(adocao.getTutor()).willReturn(tutor);
+        given(tutor.getNome()).willReturn("Rodrigo");
+        given(adocao.getData()).willReturn(LocalDateTime.now());
+
+
+        //Act
+        service.aprovar(aprovacaoAdocaoDto);
+
+
+        //Assert
+        then(adocao).should().marcarComoAprovada();
+        assertEquals(StatusAdocao.APROVADO, adocao.getStatus());
+    }
+
+    @Test
+    void deveriaEnviarEmailAoAprovarUmaAdocao(){
+
+        //Arrange
+        given(repository.getReferenceById(aprovacaoAdocaoDto.idAdocao())).willReturn(adocao);
+        given(adocao.getPet()).willReturn(pet);
+        given(pet.getAbrigo()).willReturn(abrigo);
+        given(abrigo.getEmail()).willReturn("email@example.com");
+        given(adocao.getTutor()).willReturn(tutor);
+        given(tutor.getNome()).willReturn("Rodrigo");
+        given(adocao.getData()).willReturn(LocalDateTime.now());
+
+        //Act
+        service.aprovar(aprovacaoAdocaoDto);
+
+        //Assert
+
+        then(emailService).should().enviarEmail(
+                adocao.getPet().getAbrigo().getEmail(),
+                "Adoção aprovada",
+                "Parabéns " +adocao.getTutor().getNome() +"!\n\nSua adoção do pet " +adocao.getPet().getNome() +", solicitada em " +adocao.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")) +", foi aprovada.\nFavor entrar em contato com o abrigo " +adocao.getPet().getAbrigo().getNome() +" para agendar a busca do seu pet."
+        );
+    }
+
+
+    @Test
+    void deveriaReprovarUmaAdocao(){
+        //Arrange
+        given(repository.getReferenceById(aprovacaoAdocaoDto.idAdocao())).willReturn(adocao);
+        given(adocao.getPet()).willReturn(pet);
+        given(pet.getAbrigo()).willReturn(abrigo);
+        given(abrigo.getEmail()).willReturn("email@example.com");
+        given(adocao.getTutor()).willReturn(tutor);
+        given(tutor.getNome()).willReturn("Rodrigo");
+        given(adocao.getData()).willReturn(LocalDateTime.now());
+
+        //Act
+        service.reprovar(reprovacaoAdocaoDto);
+
+
+        //
+        then(adocao).should().marcarComoReprovada(reprovacaoAdocaoDto.justificativa());
+        assertEquals(StatusAdocao.REPROVADO,adocao.getStatus());
+    }
+
+    @Test
+    void deveriaEnviarEmailAoReprovarUmaAdocao(){
+        //Arrange
+        given(repository.getReferenceById(aprovacaoAdocaoDto.idAdocao())).willReturn(adocao);
+        given(adocao.getPet()).willReturn(pet);
+        given(pet.getAbrigo()).willReturn(abrigo);
+        given(abrigo.getEmail()).willReturn("email@example.com");
+        given(adocao.getTutor()).willReturn(tutor);
+        given(tutor.getNome()).willReturn("Rodrigo");
+        given(adocao.getData()).willReturn(LocalDateTime.now());
+
+        //Act
+        service.reprovar(reprovacaoAdocaoDto);
+
+        //Assert
+
+        then(emailService).should().enviarEmail(
+                adocao.getPet().getAbrigo().getEmail(),
+                "Solicitação de adoção",
+                "Olá " +adocao.getTutor().getNome() +"!\n\nInfelizmente sua adoção do pet " +adocao.getPet().getNome() +", solicitada em " +adocao.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")) +", foi reprovada pelo abrigo " +adocao.getPet().getAbrigo().getNome() +" com a seguinte justificativa: " +adocao.getJustificativaStatus()
+        );
+    }
+
 }

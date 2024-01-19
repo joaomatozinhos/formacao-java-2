dd0c9566892581f92e599b76d9da964cf88a7bd2
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2024-01-19 17:38:39 -0300

    conceito de fabrica no clean architecture

---
+++ clean_architecture/escola/src/main/java/br/com/alura/escola/aluno/FabricaDeAluno.java
@@ -0,0 +1,26 @@
+package br.com.alura.escola;
+
+public class FabricaDeAluno {
+
+	private Aluno aluno;
+
+	public FabricaDeAluno comNomeCpfEmail(String nome, String cpf, String email) {
+		this.aluno = new Aluno(new Cpf(cpf), nome, new Email(email));
+		return this;
+	}
+
+	public FabricaDeAluno comTelefone(String ddd, String numero) {
+		this.aluno.adicionarTelefone(ddd, numero);
+		return this;
+	}
+
+	public Aluno criar() {
+		return this.aluno;
+	}
+
+	public static void main(String[] args) {
+		FabricaDeAluno fabrica = new FabricaDeAluno();
+		Aluno aluno = fabrica.comNomeCpfEmail("", "", "").comTelefone("", "").comTelefone("", "").criar();
+	}
+
+}

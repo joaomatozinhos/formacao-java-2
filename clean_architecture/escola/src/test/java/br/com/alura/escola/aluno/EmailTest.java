0a9f43a739946fc5e51b4e7719c6764ca410aaa5
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2024-01-19 17:42:44 -0300

    organizacao das classes em pacotes

--- clean_architecture/escola/src/test/java/br/com/alura/escola/aluno/EmailTest.java
+++ clean_architecture/escola/src/test/java/br/com/alura/escola/aluno/EmailTest.java
@@ -1,8 +1,10 @@
-package escola;
+package br.com.alura.escola.aluno;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+
+import br.com.alura.escola.aluno.Email;
 
 class EmailTest {
 

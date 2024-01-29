fe6d9891aebdd85ab1c54e97b374e6655d3e8aeb
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2024-01-29 14:33:09 -0300

    implementar, emitir e reagir a um evento de domínio

--- domain_driven_design/escola/src/main/java/br/com/alura/escola/academico/aplicacao/aluno/matricular/MatricularAlunoDto.java
+++ domain_driven_design/escola/src/main/java/br/com/alura/escola/academico/aplicacao/aluno/matricular/MatricularAlunoDto.java
@@ -7,20 +7,17 @@ import br.com.alura.escola.dominio.aluno.Email;
 public class MatricularAlunoDto {
 
 	private String nomeAluno;
-	private String cpfAluno;
+	private CPF cpfAluno;
 	private String emailAluno;
-	
-	public MatricularAlunoDto(String nomeAluno, String cpfAluno, String emailAluno) {
+
+	public MatricularAlunoDto(String nomeAluno, CPF cpfAluno, String emailAluno) {
 		this.nomeAluno = nomeAluno;
 		this.cpfAluno = cpfAluno;
 		this.emailAluno = emailAluno;
 	}
 
 	public Aluno criarAluno() {
-		return new Aluno(
-				new CPF(cpfAluno), 
-				nomeAluno, 
-				new Email(emailAluno));
+		return new Aluno(cpfAluno, nomeAluno, new Email(emailAluno));
 	}
-	
+
 }

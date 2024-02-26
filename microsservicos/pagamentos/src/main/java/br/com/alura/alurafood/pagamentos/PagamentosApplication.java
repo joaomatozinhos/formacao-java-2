9af96faf3171d7e9bbfebcdb0a1432cc01cb8865
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2024-02-26 10:26:22 -0300

    registrar microsservico de pagamentos no eureka

--- microsservicos/pagamentos/src/main/java/br/com/alura/alurafood/pagamentos/PagamentosApplication.java
+++ microsservicos/pagamentos/src/main/java/br/com/alura/alurafood/pagamentos/PagamentosApplication.java
@@ -3,6 +3,7 @@ package br.com.alura.alurafood.pagamentos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PagamentosApplication {
 
